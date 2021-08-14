8152a8baa9d5881e341e3e7dac349fc8003ec5fd
udincrab <sysfdn@gmail.com>
2021-08-14 21:15:15 +0700

    Update some fitures

--- 01_random_app/lib/main.dart
+++ 01_random_app/lib/main.dart
@@ -16,38 +16,46 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   List<String> tempatNgopi = [
     'Pandan',
+    'Kemiri',
     'Pacet',
     'Bendo',
+    'Batu',
+    'Malang',
     'Mojosari',
     'Mojokerto',
   ];
 
-  int currentIndex = 0;
+  int currentIndex = -1;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        title: "Random Caffe",
         home: Scaffold(
+          appBar: AppBar(
+            title: Text("Random Caffe"),
+          ),
           body: Center(
               child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text('Mau ngopi dimana?'),
-              Text(
-                tempatNgopi[currentIndex],
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
+              if (currentIndex != -1)
+                Text(
+                  tempatNgopi[currentIndex],
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
               Padding(padding: EdgeInsets.only(top: 20)),
-              TextButton(
-                style: TextButton.styleFrom(
-                    padding: EdgeInsets.all(16.0),
-                    primary: Colors.blue,
-                    textStyle: TextStyle(fontSize: 20.0)),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue, // background
+                  onPrimary: Colors.white, // foreground
+                ),
                 onPressed: () {
                   updateCurrentIndex();
                 },
-                child: Text("Acak"),
+                child: Text('Acak'),
               ),
             ],
           )),
